f7f4d674854298cdfe3d54b17de445fe510656f7
Daniel Corbellini <dcorbellini01@gmail.com>
2023-07-31 02:34:21 -0300

    Cambiar modo de visualizacion de datos a tablas en Excel

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+import random
 import sqlite3
-from pprint import pprint
-from tabulate import tabulate
+from datetime import datetime
 import pandas as pd
+import hashlib
 
 import procesar_materias
 import procesar_historia_academica
 import procesar_oferta_de_materias
+
+DEBUG = False
 
 DEFAULT_DIR = './files/'
 FILE_MATERIAS = 'materias.htm'
@@ -17,7 +20,8 @@ TABLE_OFERTA = 'OFERTA'
 TABLE_CORRELATIVAS = 'CORRELATIVAS'
 TABLE_MATERIAS = 'MATERIAS'
 
-OUTPUT = 'output.txt'
+OUTPUT = f'output_{datetime.now().strftime("%Y%m%d_%H%M%S")}.xlsx'
+writer = None
 
 dias = [
     'Lunes',
@@ -35,6 +39,7 @@ turnos = [
 
 
 def main(file_materias, file_historia, file_oferta):
+    global writer
     con = sqlite3.connect(DATABASE)
     cur = con.cursor()
 
@@ -45,24 +50,32 @@ def main(file_materias, file_historia, file_oferta):
     procesar_oferta_de_materias.load_table(cur, file_oferta)
     con.commit()
 
+    #   materias a la noche
+    # o a la tarde (excepto fisica)
+    # o los sabados
+    # excepto analisis matematico
+    #       y gestion de las organizaciones (porque son muchos)
     ofertas = get_ofertas(cur, '''
-        Turno="Noche" OR
-        Dia="Sabado"
+            (Turno="Noche" OR (Turno='Tarde' AND NOT Codigo=3628) OR Dia="Sabado")
+        AND NOT Codigo=3639
+        AND NOT Codigo=3644
     ''')
 
-    # limpiar output
-    with open(OUTPUT, 'w') as _:
-        pass
+    writer = pd.ExcelWriter(OUTPUT, engine='xlsxwriter')
 
-    generar_combinaciones(cur, ofertas, [3628, 3630] )
-    # print_combinacion(cur, {
-    #     'Jueves': (3679, 4900),
-    #     'Lunes': (3649, 1900),
-    #     'Martes': (3677, 2900),
-    #     'Miercoles': (3649, 3900),
-    #     'Sabado': (3628, 6300),
-    #     'Viernes': (3630, 5300)
-    # })
+    if DEBUG:
+        print_combinacion(cur, {
+            'Jueves': (3649, 1900),
+            'Lunes': (3649, 1900),
+            'Martes': (3649, 1900),
+            'Miercoles': (3649, 1900),
+            'Sabado': (3649, 1900),
+            'Viernes': (3649, 1900)
+        })
+    else:
+        generar_combinaciones(cur, ofertas, [ 3630])
+
+    writer.close()
 
 
 def generar_combinaciones(cur, ofertas, requisitos=None, combinacion=None, i=0, materias=None):
@@ -80,8 +93,8 @@ def generar_combinaciones(cur, ofertas, requisitos=None, combinacion=None, i=0, 
 
     dia = dias[i]
 
-    # Dias que no tienen ninguna materia
-    if not ofertas[dia]:
+    # Dias que no tienen ninguna materia que no haya sido tomada (o que no tengan materias)
+    if set([codigo for codigo, _ in ofertas[dia]]).issubset(materias):
         combinacion[dia] = None
         generar_combinaciones(cur, ofertas, requisitos, combinacion, i + 1, materias)
     else:
@@ -95,7 +108,12 @@ def generar_combinaciones(cur, ofertas, requisitos=None, combinacion=None, i=0, 
             materias[i] = None
 
 
+startrow = 1
+
+
 def print_combinacion(cur, combinacion):
+    global startrow
+    global writer
     nombres = []
     table = {dia: {turno: '' for turno in turnos} for dia in dias}
     for dia in dias:
@@ -109,25 +127,55 @@ def print_combinacion(cur, combinacion):
             WHERE O.Codigo={codigo} AND O.Comision={comision}
         ''')
         nombre, turno = res.fetchone()
-        table[dia][turno] = f'{nombre}\n{codigo} - {comision}'
+        table[dia][turno] = f'{nombre}\n({codigo} - {comision})'
         nombres.append(nombre)
 
     df = pd.DataFrame(table)
-    with open(OUTPUT, 'a') as file:
-        file.write(', '.join(nombres))
-        file.write('\n')
-        file.writelines(tabulate(df,
-                   headers="keys",
-                   tablefmt='grid',
-                   colalign=['center' for i in range(7)]))
-        file.write('\n\n')
-        file.write('='*120)
-        file.write('\n\n')
-    # print(tabulate(df,
-    #                headers="keys",
-    #                tablefmt='grid',
-    #                colalign=['center' for i in range(7)]))
 
+    # if DEBUG:
+    #     print(tabulate(df,
+    #                    headers="keys",
+    #                    tablefmt='grid',
+    #                    colalign=['center' for i in range(7)]))
+    # else:
+
+    st = df.style.applymap(generar_style)
+    st.to_excel(writer, 'Combinaciones', startrow=startrow, startcol=1)
+
+    # formato para columnas
+    wrap_format = writer.book.add_format({'text_wrap': True})
+    # for i in range(len(dias)):
+    #     # ajustar tamaño de columna
+    #     writer.sheets['Combinaciones'].set_column(i+2, i+2, 10, wrap_format)
+    for column in df:
+        # ajustar tamaño de columna
+        column_length = max(df[column].astype(str).map(len).max(), len(column))
+        col_idx = df.columns.get_loc(column)+2  # una columna en blanco, una columna para el turno
+        writer.sheets['Combinaciones'].set_column(col_idx, col_idx, column_length, wrap_format)
+
+    startrow += 5
+
+
+def generar_style(string):
+    if string:
+        # solo quiero hashear la parte del nombre
+        # random.seed(hash(string.split('\n')[0]))
+        # uso hashlib porque quiero que sea deterministico
+        random.seed(hashlib.md5(string.split('\n')[0].encode()).hexdigest())
+
+        # generar color claro
+        r = random.randint(120, 235)
+        g = random.randint(150, 235)
+        b = random.randint(120, 235)
+
+        # convertir a codigo hexadecimal
+        color_hex = f'#{r:02x}{g:02x}{b:02x}'
+    else:
+        color_hex = '#FFFFFF'
+
+    return (f'background-color: {color_hex}; '
+            f'text-align: center; '
+            f'vertical-align: middle; ')
 
 
 def get_ofertas(cur, condiciones=''):
